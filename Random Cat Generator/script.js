7f6fcc0871adcd265d0329df02c28cfc156cb81e
altinsadiku33 <52981056+altinsadiku33@users.noreply.github.com>
2021-07-24 00:25:22 +0200

    Add files via upload

---
+++ Random Cat Generator/script.js
@@ -0,0 +1,6 @@
+function generateCat(){
+    var image= document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "http://edgecats.net/0.2554957213083029";
+    div.appendChild(image);
+}
